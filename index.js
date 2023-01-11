b1f143eb7cb5c37c308eca68d24915fe3ea073b9
kiryong159 <kiryong159@naver.com>
2023-01-11 10:55:58 +0900

    rrr

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const inputfile = document.querySelector(".file");
+const savebtn = document.querySelector(".saveimg");
+const inputtext = document.getElementById("text");
 const eraseBtn = document.querySelector(".eraser-btn");
 const modeBtn = document.querySelector(".mode-btn");
 const destroyBtn = document.querySelector(".destroy-btn");
@@ -6,9 +9,15 @@ const lineWidthh = document.querySelector(".line-width");
 const colors = document.querySelector(".color");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+const inputfont = document.getElementById("fontsize");
+const fillfontbtn = document.querySelector(".fillfont");
+
 canvas.width = 800;
 canvas.height = 800;
+ctx.font = `${inputfont.value}px serif`;
 ctx.lineWidth = lineWidthh.value;
+ctx.lineCap = "round";
+let fillfont = false;
 let nowpainting = false;
 let Filling = false;
 
@@ -70,6 +79,59 @@ function eraserrr() {
   modeBtn.innerText = "NOW DORW MODE";
 }
 
+function uploadfile(event) {
+  const file = event.target.files[0];
+  const url = URL.createObjectURL(file);
+  const upimg = new Image();
+  upimg.src = url;
+  upimg.onload = function () {
+    ctx.drawImage(upimg, 0, 0, 800, 800);
+  };
+}
+
+function dbclick(event) {
+  const text = inputtext.value;
+  if (text !== "") {
+    if (fillfont === false) {
+      ctx.save();
+      ctx.lineWidth = 1;
+      ctx.strokeText(text, event.offsetX, event.offsetY);
+      ctx.restore();
+    } else {
+      ctx.save();
+      ctx.lineWidth = 1;
+      ctx.fillText(text, event.offsetX, event.offsetY);
+      ctx.restore();
+    }
+  }
+}
+
+function fontsizechange(event) {
+  const fontsize = event.target.value;
+  ctx.font = `${fontsize}px serif`;
+}
+
+function saveimg() {
+  const url = canvas.toDataURL();
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = "mydraw.jpg";
+  a.click();
+}
+
+function fillfontclick() {
+  if (fillfont === false) {
+    fillfont = true;
+    fillfontbtn.innerText = "현재 글씨 채우기 모드";
+  } else {
+    fillfont = false;
+    fillfontbtn.innerText = "현재 글씨 stroke 모드";
+  }
+}
+
+fillfontbtn.addEventListener("click", fillfontclick);
+inputfont.addEventListener("change", fontsizechange);
+canvas.addEventListener("dblclick", dbclick);
 canvas.addEventListener("click", filldrow);
 canvas.addEventListener("mousemove", movemouse);
 canvas.addEventListener("mousedown", startpaint);
@@ -79,7 +141,9 @@ canvas.addEventListener("mouseleave", canclepaint);
 lineWidthh.addEventListener("change", linewidthhh);
 colors.addEventListener("change", colorchan);
 
-colorOption.forEach((color) => color.addEventListener("click", changecolor));
+colorOption.forEach((varr) => varr.addEventListener("click", changecolor));
 modeBtn.addEventListener("click", modeclick);
 destroyBtn.addEventListener("click", destroy);
 eraseBtn.addEventListener("click", eraserrr);
+inputfile.addEventListener("change", uploadfile);
+savebtn.addEventListener("click", saveimg);
